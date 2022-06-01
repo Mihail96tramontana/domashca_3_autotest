8960af95a25b11cb6f4c8d028bce646017a7e8e5
parnishca <xelysqgbhju@gmail.com>
2022-06-01 21:39:47 +0300

    code test

--- src/test/java/domashcka_3/HomeworkThreeTest.java
+++ src/test/java/domashcka_3/HomeworkThreeTest.java
@@ -48,9 +48,16 @@ public class HomeworkThreeTest {
         $("#submit").click();
 
         //проверки
-        $(".modal-content").shouldHave(text("Mihail Lubeznow"), text("mihail@mail.ru"),
-                text("Male"), text("1234567890"), text("10 April,2020"), text("Maths"),
-                text("Music"), text("1.jpg"), text("Miami"), text("NCR Noida"));
+        $(".modal-content").shouldHave(text("Mihail Lubeznow"),
+                text("mihail@mail.ru"),
+                text("Male"),
+                text("1234567890"),
+                text("10 April,2020"),
+                text("Maths"),
+                text("Music"),
+                text("1.jpg"),
+                text("Miami"),
+                text("NCR Noida"));
     }
 }
 
